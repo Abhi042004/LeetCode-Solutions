9dbe6b0adc75eb19639f74921157dd374eadd28a
Abhishek <abhi042004@outlook.com>
2026-01-03 18:11:39 +0530

    Solved Two Sum using optimal HashMap approach

--- 1.Arrays/Easy/TwoSum.java
+++ 1.Arrays/Easy/TwoSum.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
 
@@ -19,22 +20,73 @@ public class TwoSum {
     // Input: nums = [3,3], target = 6
     // Output: [0,1]
 
-        int[] nums = {3, 2, 4};
-        int target = 6;
-        int[] arr = new int[2];
+        // int[] nums = {3,2,4};
+        // int target = 6;
+        // int[] arr = new int[2];
 
-        for (int i = 0; i < nums.length-1; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                System.out.println(nums[i]+nums[j]);
-                if(nums[i]+nums[j] == target){
-                    arr[0] = nums[i];
-                    arr[1] = nums[j];
-                    System.out.println(arr[0]);
-                    System.out.println(arr[1]);
-                }
-            }
-        }
-        System.out.println(Arrays.toString(arr));
+        //Brute Force Solution
+        // int flag = 0;
+        
+        // for(int i = 0;i<nums.length;i++){
 
+        //     for(int j = 0;j<nums.length;j++){
+        //         if(i == j){continue;}
+        //         if(nums[i]+nums[j]==target){
+        //             arr[0] = i;
+        //             arr[1] = j;
+        //             flag = 1;
+        //         }
+        //     }
+        //     if(flag == 1){
+        //         break;
+        //     }
+        // }
+        
+        // System.out.println(Arrays.toString(arr));
+
+    //Better Solution
+    //     HashMap<Integer,Integer> map = new HashMap<>();
+    //     map.put(nums[0], 0);
+    //     for (int i = 1; i < nums.length; i++) {
+            
+    //         if (map.containsKey(target - nums[i])) {
+    //             arr[0] = map.get(target - nums[i]);
+    //             arr[1] = i;
+    //             break;
+    //         }
+    //         map.put(nums[i],i);
+    //     }
+    //     System.out.println(Arrays.toString(arr));
+    // }
+
+    // No optimal solution for this variation only better one is there this
+    // is for another variation
+//    int[] nums = {2, 5, 5, 11};
+//    int target = 10;
+//    Arrays.sort(nums);
+
+//     int[] arr = new int[2];
+
+//     int i = 0;
+//     int j = nums.length-1;
+
+//     while (i!=j) {
+
+//         int sum = nums[i]+nums[j];
+
+//         if (sum==target) {
+//             arr[0] = i;
+//             arr[1] = j;
+//             break;
+//         }
+//         if (sum>target) {
+//             j--;
+//         }
+//         else{
+//             i++;
+//         }
+       
+//         }
+//         System.out.println(Arrays.toString(arr));
     }
 }
