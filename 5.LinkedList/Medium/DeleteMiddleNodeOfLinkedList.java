7b85d160aec3c656ad4aca6964777b875e6e94da
Abhishek <abhi042004@outlook.com>
2026-03-06 01:17:39 +0530

    Delete Middle Node Problem has been solved

---
+++ 5.LinkedList/Medium/DeleteMiddleNodeOfLinkedList.java
@@ -0,0 +1,66 @@
+public class DeleteMiddleNodeOfLinkedList {
+
+    // Definition for singly-linked list
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    // Function to delete the middle node
+    public static ListNode deleteMiddle(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // remove middle node
+        prev.next = slow.next;
+
+        return head;
+    }
+
+    // Helper function to print the linked list
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        // Creating linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original Linked List:");
+        printList(head);
+
+        head = deleteMiddle(head);
+
+        System.out.println("After Deleting Middle Node:");
+        printList(head);
+    }
+}
